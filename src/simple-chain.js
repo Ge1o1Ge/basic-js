66fb5f8073ac4a0cfc3238c740fa939fc89675e2
Georg II <118926055+Ge1o1Ge@users.noreply.github.com>
2023-04-17 03:51:15 +0200

    feat: make chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  length: 0,
+  elements: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.elements.push(String(value));
+    this.length += 1;
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(ind) {
+    try {
+      (() => {
+        if (!this.elements[ind - 1]) {
+          throw new Error('You can\'t remove incorrect link!');
+        }
+        this.length -= 1;
+        this.elements.splice(ind - 1, 1);
+      })();
+      return this
+    } catch (er) {
+      this.elements = [];
+      this.length = 0
+      throw er;
+    }  
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.elements.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const result = '( ' + this.elements.join(' )~~( ') + ' )'
+    this.elements = [];
+    this.length = 0
+    return result
+  },
 };
+
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain())
+console.log('( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )')
 
 module.exports = {
   chainMaker
