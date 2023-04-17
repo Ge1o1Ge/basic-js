48bc6f812c7bba9118545fd2071cb20d5a193fd7
Georg II <118926055+Ge1o1Ge@users.noreply.github.com>
2023-04-17 07:53:42 +0200

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let resStr = '';
+    let prob = 0;
+    try {
+      (() => {
+        str = str.toLowerCase()
+        key = key.toLowerCase()
+
+        for (let i = 0; i < str.length; i++) {
+          if (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 122) {
+            let j = (i - prob) % key.length;
+            resStr += String.fromCharCode((str.charCodeAt(i) + key.charCodeAt(j) - 97 * 2) % 26 + 65);
+          } else {
+            resStr += str[i];
+            prob += 1
+          }
+        }
+      })()
+      if (this.reverse) {
+        return resStr
+      } else {
+        return resStr.split('').reverse().join('');
+      }
+
+    } catch (er) {
+      throw er
+    }
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let resStr = '';
+    let prob = 0;
+    try {
+      (() => {
+        str = str.toUpperCase()
+        key = key.toLowerCase()
+
+        for (let i = 0; i < str.length; i++) {
+          if (65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 90) {
+            let j = (i - prob) % key.length;
+            resStr += String.fromCharCode((str.charCodeAt(i) - key.charCodeAt(j) + 58) % 26 + 65);
+          } else {
+            resStr += str[i];
+            prob += 1
+          }
+        }
+      })();
+      if (this.reverse) {
+        return resStr
+      } else {
+        return resStr.split('').reverse().join('');
+      }
+    } catch (er) {
+      throw er
+    }
   }
 }
+
+let p = new VigenereCipheringMachine();
+let d = new VigenereCipheringMachine(false);
+console.log(p.encrypt('Samelengthkey', 'Samelengthkey'))
+console.log(d.encrypt('Samelengthkey', 'Samelengthkey'))
+
 
 module.exports = {
   VigenereCipheringMachine
