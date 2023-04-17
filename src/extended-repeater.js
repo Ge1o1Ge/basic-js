a84ff369de33f2b1def20ce28de7ddaae1b1dcfc
Georg II <118926055+Ge1o1Ge@users.noreply.github.com>
2023-04-17 05:58:52 +0200

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newstr = '';
+  let substr;
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  } 
+
+  if (options.separator === undefined) {
+    options.separator = '+';
+  } 
+
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+
+  substr = String(str) + String(options.addition);
+
+  for (let j = 1; j <options.additionRepeatTimes; j++) {
+    substr += String(options.additionSeparator) + String(options.addition);
+  }
+  
+
+  newstr = substr;
+  for (let i = 1; i < options.repeatTimes; i++) {
+    newstr += String(options.separator) + substr;
+  }
+
+  return newstr
 }
 
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }))
+console.log('nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null')
 module.exports = {
   repeater
 };
